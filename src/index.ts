787678ce537c97daf04ffd168e57b9ad5e2ed32b
imhyperion <imhyperion@users.noreply.github.com>
2021-01-12 13:47:49 -0800

    added front end

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("hi there");
+import { User } from "./models/User";
+import { UserForm } from "./views/UserForm";
+
+const user = User.buildUser({name: 'NAME', age: 20});
+
+const userForm = new UserForm(
+    document.getElementById('root'), 
+    user
+);
+
+userForm.render();
